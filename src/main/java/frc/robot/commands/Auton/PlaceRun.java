f158e914d24eef769ed9bac35404cc7c21fcc48c
Karl Fleischmann <karl_fleischmann@hotmail.com>
2023-02-18 09:32:46 -0500

    Path Planner Tweaks

--- src/main/java/frc/robot/commands/Auton/PlaceRun.java
+++ src/main/java/frc/robot/commands/Auton/PlaceRun.java
@@ -4,11 +4,15 @@
 
 package frc.robot.commands.Auton;
 
+import java.util.HashMap;
+
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.Arm.LowerArm;
@@ -22,34 +26,39 @@ import frc.robot.subsystems.SwerveSubsystem;
 public class PlaceRun extends SequentialCommandGroup {
   /** Creates a new PlaceRun. */
   public PlaceRun(
-    ArmSubsystems armSubsystem, 
-    SwerveSubsystem swerveSub,
-    PathPlannerTrajectory traj,
-    boolean isFirstPath
-   ) {
+      ArmSubsystems armSubsystem,
+      SwerveSubsystem swerveSub,
+      PathPlannerTrajectory traj,
+      boolean isFirstPath) {
+
+    HashMap<String, Command> eventMap = new HashMap<>();
+    eventMap.put("marker1", new PrintCommand("Passed marker 1"));
+    eventMap.put("intakeDown", new RaiseArm(armSubsystem));
+
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new InstantCommand(() -> {
-      // Reset odometry for the first path you run during auto
-      if(isFirstPath){
-          swerveSub.resetOdometry(traj.getInitialHolonomicPose());
-      }
-    }),
-    new RaiseArm(armSubsystem), 
-    new LowerArm(armSubsystem), 
-    new SimpleAuton(swerveSub),
-    new PPSwerveControllerCommand(
-        traj, 
-        swerveSub::getPose, // Pose supplier
-        Constants.SwerveDrivetrain.SWERVE_KINEMATICS, // SwerveDriveKinematics
-        new PIDController(0, 0, 0), // X controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
-        new PIDController(0, 0, 0), // Y controller (usually the same values as X controller)
-        new PIDController(0, 0, 0), // Rotation controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
-        swerveSub::setModuleStates, // Module states consumer
-        true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-        swerveSub // Requires this drive subsystem
-    ) 
-    );
+        new InstantCommand(() -> {
+          // Reset odometry for the first path you run during auto
+          if (isFirstPath) {
+            swerveSub.resetOdometry(traj.getInitialHolonomicPose());
+          }
+        }),
+        new RaiseArm(armSubsystem),
+        new LowerArm(armSubsystem),
+        new PPSwerveControllerCommand(
+            traj,
+            swerveSub::getPose, // Pose supplier
+            Constants.SwerveDrivetrain.SWERVE_KINEMATICS, // SwerveDriveKinematics
+            new PIDController(0, 0, 0), // X controller. Tune these values for your robot. Leaving them 0 will only use
+                                        // feedforwards.
+            new PIDController(0, 0, 0), // Y controller (usually the same values as X controller)
+            new PIDController(0, 0, 0), // Rotation controller. Tune these values for your robot. Leaving them 0 will
+                                        // only use feedforwards.
+            swerveSub::setModuleStates, // Module states consumer
+            true, // Should the path be automatically mirrored depending on alliance color.
+                  // Optional, defaults to true
+            swerveSub // Requires this drive subsystem
+        ));
   }
 }
